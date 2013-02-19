4387ef5a1bd8075d9a7512e09f8987b0a9b74335
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2013-02-19 11:36:45 +0900

    refactor: use custom matcher

--- spec/android_market_spec.rb
+++ spec/android_market_spec.rb
@@ -56,15 +56,13 @@ describe AndroidMarket do
   describe "#get_top_selling_free_apps_in_category" do
     subject { AndroidMarket.get_top_selling_free_apps_in_category(category, position, language) }
 
-    it{ should be_an_instance_of Array }
-    its(:first){ should be_an_instance_of AndroidMarketApplication }
+    it{ should array_instance_of AndroidMarketApplication }
   end
 
   describe "#get_developer_app_list" do
     subject { AndroidMarket.get_developer_app_list(developer_name, position, language) }
 
-    it{ should be_an_instance_of Array }
-    its(:first){ should be_an_instance_of AndroidMarketApplication }
+    it{ should array_instance_of AndroidMarketApplication }
   end
 
 end
