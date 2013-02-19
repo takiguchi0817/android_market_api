64d95c2e3b8210ea390a452fb96beee1814de746
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2013-02-19 14:42:00 +0900

    apply

--- lib/android_market_api/util.rb
+++ lib/android_market_api/util.rb
@@ -14,15 +14,19 @@ module AndroidMarketApi
 
     module_function :sanitize
 
-    def get_content(uri, headers = {})
-      url = URI.parse(uri)
-      req = Net::HTTP::Get.new(uri)
-      headers.each do |name, value|
-        req[name] = value
-      end
-      res = Net::HTTP.start(url.host, url.port) {|http|
+    def get_content(url, headers = {})
+      uri = URI.parse(url)
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true  if url.start_with?("https://")
+
+      res = http.start {|http|
+        req = Net::HTTP::Get.new(url)
+        headers.each do |name, value|
+          req[name] = value
+        end
         http.request(req)
       }
+
       res.body
     end
 
