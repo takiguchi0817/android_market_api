550ae18d0ead85b605531cd6cd93356599c44631
sueyoshi_go <go.sueyoshi@drecom.co.jp>
2013-02-20 20:48:46 +0900

    impl rankings

--- lib/android_market_api/android_market.rb
+++ lib/android_market_api/android_market.rb
@@ -73,6 +73,24 @@ class AndroidMarket
       get_apps_in_carousel(url, xpath, language)
     end
 
+    def get_overall_topgrossing_apps(position,language='en')
+      url = "https://play.google.com/store/apps/collection/topgrossing?start=#{position-1}&hl=#{language}"
+      xpath = "//div[@class='num-pagination-page']//li[@class='goog-inline-block']"
+      get_apps_in_carousel(url, xpath, language)
+    end
+
+    def get_overall_topselling_new_paid_apps(position,language='en')
+      url = "https://play.google.com/store/apps/collection/topselling_new_paid?start=#{position-1}&hl=#{language}"
+      xpath = "//div[@class='num-pagination-page']//li[@class='goog-inline-block']"
+      get_apps_in_carousel(url, xpath, language)
+    end
+
+    def get_overall_topselling_new_free_apps(position,language='en')
+      url = "https://play.google.com/store/apps/collection/topselling_new_free?start=#{position-1}&hl=#{language}"
+      xpath = "//div[@class='num-pagination-page']//li[@class='goog-inline-block']"
+      get_apps_in_carousel(url, xpath, language)
+    end
+
     def get_developer_app_list(developer_name, position, language='en')
       url="https://play.google.com/store/apps/developer?id=#{CGI.escape(developer_name)}&start=#{position-1}&hl=#{language}"
       xpath = "li[@class='goog-inline-block']"
